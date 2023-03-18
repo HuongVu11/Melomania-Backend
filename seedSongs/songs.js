7d2fd7bcb5b7b638f5b572b54627f1c6ab752993
anastasiia92 <nastia.dmytryshyna@gmail.com>
2023-03-17 23:25:32 -0400

    backend login

---
+++ seedSongs/songs.js
@@ -0,0 +1,38 @@
+const data = [
+    {   title: 'Remeber The Time',
+        artist: 'Micheal Jackson',
+        album: 'Dangerous',
+        image: 'https://images.genius.com/984f3eb1d4ff07878238a3041131553f.1000x1000x1.jpg',
+        link:'https://www.youtube.com/watch?v=1aJq2yoO6GQ'
+    },
+    {
+        title: 'We Found Love ft. Calvin Harris',
+        artist: 'Rihanna',
+        album: 'Talk That Talk',
+        image: 'https://images.genius.com/cd00f82bc2ec2e39d8faa73f75331aff.1000x1000x1.jpg',
+        link: 'https://www.youtube.com/watch?v=GchEVSx9XEA'
+    },
+    {
+        title: 'Take Me To Church',
+        artist: 'Hozier',
+        album: 'Hozier',
+        image: 'https://dvfnvgxhycwzf.cloudfront.net/media/SharedImage/imageFull/.fsl3podU/SharedImage-11407.jpg?t=72c2ab161745014ac555',
+        link: 'https://www.youtube.com/watch?v=1kYgXOrA3Jc'
+    },
+    {
+        title: 'Die For You',
+        artist: 'The Weeknd',
+        album: 'Starboy',
+        image: 'https://pics.filmaffinity.com/the_weeknd_ft_daft_punk_starboy-789136144-large.jpg',
+        link: 'https://www.youtube.com/watch?v=QLCpqdqeoII'
+    },
+    {
+        title: 'Nights',
+        artist: 'Frank Ocean',
+        album: 'Blonde',
+        image: 'https://www.slantmagazine.com/wp-content/uploads/2016/08/blonde.jpg',
+        link: 'https://www.youtube.com/watch?v=r4l9bFqgMaQ'
+    }
+]
+
+module.exports = data
--- server.js
+++ server.js
@@ -2,18 +2,38 @@
 
 // get .env variables
 require("dotenv").config();
-// pull PORT from .env, give default value of 3000
+// pull PORT from .env, give default value of 4000
 // pull DATABASE_URL from .env
-const { PORT = 3000, DATABASE_URL } = process.env;
+const { DATABASE_URL, PORT=4000 } = process.env;
 // import express
 const express = require("express");
 // create application object
 const app = express();
 // import mongoose
 const mongoose = require("mongoose");
+// import songsRouter
+const songsController = require('./controllers/songs')
 
 
-// DATABASE CONNECTION
+const cors = require('cors')
+const morgan = require('morgan');
+
+const db = mongoose.connection
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+
+// use Router
+app.use('/songs', songsController)
+
+
+// ROUTES
+// create a test route
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
 
 // Establish Connection
 mongoose.connect(DATABASE_URL, {
@@ -21,20 +41,13 @@ mongoose.connect(DATABASE_URL, {
   useNewUrlParser: true,
 });
 // Connection Events
-mongoose.connection
-  .on("open", () => console.log("Your are connected to mongoose"))
-  .on("close", () => console.log("Your are disconnected from mongoose"))
-  .on("error", (error) => console.log(error));
+db.on("open", () => console.log("Your are connected to mongoose"))
+db.on("close", () => console.log("Your are disconnected from mongoose"))
+db.on("error", (error) => console.log(error));
 
-
-// ROUTES
-
-// create a test route
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
 
 
 // LISTENER
 
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
+
